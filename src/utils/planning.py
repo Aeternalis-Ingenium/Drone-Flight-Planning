f27d3c3197aafcf80aa719e410b83c59d4d47fee
N. L. <aeternalis-ingenium@proton.me>
2024-11-15 15:24:21 +0100

    feat(planning): create planning algorithms: bfs, dfs, a*

---
+++ src/utils/planning.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+from enum import Enum
+
+
+class Action(Enum):
+    LEFT = (0, -1, 1)
+    RIGHT = (0, 1, 1)
+    UP = (-1, 0, 1)
+    DOWN = (1, 0, 1)
+    
+    def __str__(self):
+        if self == self.UP:
+            return "^"
+        elif self == self.DOWN:
+            return "v"
+        elif self == self.LEFT:
+            return "<"
+        elif self == self.RIGHT:
+            return ">"
+
+    @property
+    def cost(self):
+        return self.value[2]
+    
+    @property
+    def delta(self):
+        return (self.value[0], self.value[1])
+
+
+def valid_actions(grid, current_node):
+    """
+    Returns a list of valid actions given a grid and current node.
+    """
+
+    valid = [Action.UP, Action.LEFT, Action.RIGHT, Action.DOWN]
+    n, m = grid.shape[0] - 1, grid.shape[1] - 1
+    x, y = current_node
+    
+    if x - 1 < 0 or grid[x-1, y] == 1:
+        valid.remove(Action.UP)
+    if x + 1 > n or grid[x+1, y] == 1:
+        valid.remove(Action.DOWN)
+    if y - 1 < 0 or grid[x, y-1] == 1:
+        valid.remove(Action.LEFT)
+    if y + 1 > m or grid[x, y+1] == 1:
+        valid.remove(Action.RIGHT)
+        
+    return valid
+
+
+def visualize_path(grid, path, start):
+    sgrid = np.zeros(np.shape(grid), dtype=str)
+    sgrid[:] = ' '
+    sgrid[grid[:] == 1] = 'O'
+    
+    pos = start
+    
+    for a in path:
+        da = a.value
+        sgrid[pos[0], pos[1]] = str(a)
+        pos = (pos[0] + da[0], pos[1] + da[1])
+    sgrid[pos[0], pos[1]] = 'G'
+    sgrid[start[0], start[1]] = 'S'  
+    return sgrid
